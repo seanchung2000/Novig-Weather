d5ca8c7b4e1b4fc375694ed0b28ad1e8966506c0
Sean Chung <seanchung2000@berkeley.edu>
2024-05-03 20:24:37 -0400

    chart changes

--- src/components/WeatherChart.js
+++ src/components/WeatherChart.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
-import { Chart } from 'chart.js';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 
 // Child component for building weather chart within timeOfDay period and sending it up to parent component (Weather)
